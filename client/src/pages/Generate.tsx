3e416805f5c2ba332d0d5f1023a55c776b301271
ishapanchal07 <isha7112003@gmail.com>
2026-01-09 14:31:57 +0530

    typo error sloved

--- client/src/pages/Generate.tsx
+++ client/src/pages/Generate.tsx
@@ -1,9 +1,37 @@
-
+import { useParams } from "react-router-dom"
+import { useState } from "react";
+import { IThumbnail } from "../assets/assets";
+import SoftBackdrop from "../components/SoftBackdrop";
 
 
 const Generate = () => {
+    const {id} = useParams();
+    const [title, setTitle] = useState('')
+    const [additionalDetails, setAdditionalDetails] = useState('')
+
+    const [thumbnail, setThumbnail] = useState<IThumbnail | null>(null)
+    const [loading, setLoading] = useState(false)
+
     return (
-    <div></div>
+    <>
+    <SoftBackdrop />
+    <div className="pt-24 min-h-screen">
+        <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pb-28 lg:pb-8">
+            <div className="grid lg:grid-cols-[400px_1fr] gap-8">
+                {/* LEFT PANEL */}
+                <div className={`space-y-6 ${id && 'pointer-events-none'}`}>
+                    <div className="p-6 rounded-2xl bg-white/8 border border-white/12 shadow-xl space-y-6">
+                    <div>
+                        <h2>Create Your Thumbnail</h2>
+                    </div>
+                    </div>
+                </div>
+                {/* RIGHT PANEL */}
+                <div></div>
+            </div>
+        </main>
+    </div>
+    </>
     )
 }
 
