907fa37711239aff5682f364e0acb66d15b9b02c
ishapanchal07 <isha7112003@gmail.com>
2026-01-22 14:22:37 +0530

    frontend created successfully

--- client/src/pages/YtPreview.tsx
+++ client/src/pages/YtPreview.tsx
@@ -1,21 +1,26 @@
-import { useSearchParams } from 'react-router-dom'
-import {yt_html} from '../assets/assets'
+import { useSearchParams } from "react-router-dom";
+import { yt_html } from "../assets/assets";
 
 const YtPreview = () => {
+  const [searchParams] = useSearchParams();
 
-    const [] = useSearchParams()
+  const thumbnail_url = searchParams.get("thumbnail_url");
+  const title = searchParams.get("title");
 
-    const thumbnail_url = useSearchParams.get('thumbnail_url')
-    const title = useSearchParams.get('title')
+  const new_html = yt_html
+    .replace("%%THUMBNAIL_URL%%", thumbnail_url ?? "")
+    .replace("%%TITLE%%", title ?? "");
 
-    const new_html = yt_html.replace("%%THUMBNAIL_URL%%", thumbnail_url!).replace("%%TITLE%%", title!);
+  return (
+    <div className="fixed inset-0 z-100 bg-black">
+      <iframe
+        srcDoc={new_html}
+        width="100%"
+        height="100%"
+        allowFullScreen
+      />
+    </div>
+  );
+};
 
-
-    return (
-        <div className='fixed inset-0 z-100 bg-black'>
-            <iframe srcDoc={new_html} width="100%" height="100%" allowFullScreen></iframe>
-        </div>
-    )
-}
-
-export default YtPreview
+export default YtPreview;
