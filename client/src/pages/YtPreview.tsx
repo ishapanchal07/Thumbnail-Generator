a893c2a264963ca67ee5cf5f9dfbc46c76a73b80
ishapanchal07 <isha7112003@gmail.com>
2026-01-22 14:13:10 +0530

    preview added

--- client/src/pages/YtPreview.tsx
+++ client/src/pages/YtPreview.tsx
@@ -1,8 +1,20 @@
-
+import { useSearchParams } from 'react-router-dom'
+import {yt_html} from '../assets/assets'
 
 const YtPreview = () => {
+
+    const [] = useSearchParams()
+
+    const thumbnail_url = useSearchParams.get('thumbnail_url')
+    const title = useSearchParams.get('title')
+
+    const new_html = yt_html.replace("%%THUMBNAIL_URL%%", thumbnail_url!).replace("%%TITLE%%", title!);
+
+
     return (
-        <div></div>
+        <div className='fixed inset-0 z-100 bg-black'>
+            <iframe srcDoc={new_html} width="100%" height="100%" allowFullScreen></iframe>
+        </div>
     )
 }
 
